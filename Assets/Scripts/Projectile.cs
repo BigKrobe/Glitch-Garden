5930fcb158522b584d7e63619888faad5b71d4c5
BigKrobe <natebrks@gmail.com>
2020-10-07 05:12:55 +1000

    Projectile Script Added

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0, 5)] public float projectileSpeed = 1;
+    [Range(0, 5)] public float projectileRotation = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+               
+    }
+}
